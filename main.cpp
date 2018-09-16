5fadc8272bf2ce97f25b61f711c04b0f0c5bf38d
Alex <atags22@gmail.com>
2018-09-16 17:00:04 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "mbed.h"
+#include "rtos.h"
+
+DigitalOut led1(LED1);
+PwmOut led2(LED2);
+Thread thread;
+
+void led2_thread() {
+  led2.period(.020); //50Hz
+  while (true) {
+    led2.pulsewidth_us(1000);
+    Thread::wait(1000);
+    led2.pulsewidth_us(2000);
+    Thread::wait(1000);
+  }
+}
+
+int main() {
+  thread.start(led2_thread);
+
+  while (true) {
+    led1 = !led1;
+    Thread::wait(500);
+  }
+}
